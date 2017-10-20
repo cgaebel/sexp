5cd9a188e07fc52b68510ada7463ba8f819aa2be
Vee Satayamas <vsatayamas@gmail.com>
2017-10-21 00:36:49 +0700

    make distinction between symbol and string

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ use std::str::{self, FromStr};
 #[allow(missing_docs)]
 pub enum Atom {
   S(String),
+  Y(String),
   K(String),
   I(i64),
   F(f64),
@@ -150,8 +151,12 @@ fn atom_of_string(s: String) -> Atom {
   if s.starts_with(":") {
     return Atom::K(s)
   }
-    
-  Atom::S(s)
+
+  if s.starts_with("\"") {
+    return Atom::S(s)
+  }
+
+  Atom::Y(s)  
 }
 
 // returns the char it found, and the new size if you wish to consume that char
@@ -290,6 +295,11 @@ pub fn atom_s(s: &str) -> Sexp {
   Sexp::Atom(Atom::S(s.to_owned()))
 }
 
+/// Constructs an atomic s-expression (symbol) from a string.
+pub fn atom_y(y: &str) -> Sexp {
+  Sexp::Atom(Atom::Y(y.to_owned()))
+}
+
 /// Constructs an atomic s-expression (keyword) from a string.
 pub fn atom_k(k: &str) -> Sexp {
   Sexp::Atom(Atom::K(k.to_owned()))
@@ -351,7 +361,8 @@ impl fmt::Display for Atom {
   fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
     match *self {
       Atom::S(ref s) => write!(f, "{}", quote(s)),
-      Atom::K(ref k) => write!(f, ":{}", k),  
+      Atom::Y(ref y) => write!(f, "{}", y),  
+      Atom::K(ref k) => write!(f, ":{}", k),        
       Atom::I(i)     => write!(f, "{}", i),
       Atom::F(d)     => write!(f, "{}", d),
     }
@@ -390,7 +401,7 @@ impl fmt::Debug for Sexp {
 fn test_hello_world() {
   assert_eq!(
     parse("(hello -42\n\t  -4.0 \"world\") ; comment").unwrap(),
-    list(&[ atom_s("hello"), atom_i(-42), atom_f(-4.0), atom_s("world") ]));
+    list(&[ atom_y("hello"), atom_i(-42), atom_f(-4.0), atom_s("world") ]));
 }
 
 #[test]
@@ -407,6 +418,12 @@ fn test_keyword() {
     list(&[ atom_k(":key") ]));
 }
 
+#[test]
+fn test_symbol() {
+  assert_eq!(
+    parse("(sym)").unwrap(),
+    list(&[ atom_y("sym") ]));
+}
 
 #[test]
 fn test_pp() {
@@ -420,8 +437,8 @@ fn test_pp() {
 fn test_tight_parens() {
     let s = "(hello(world))";
     let sexp = parse(s).unwrap();
-    assert_eq!(sexp, Sexp::List(vec![Sexp::Atom(Atom::S("hello".into())),
-                                     Sexp::List(vec![Sexp::Atom(Atom::S("world".into()))])]));
+    assert_eq!(sexp, Sexp::List(vec![Sexp::Atom(Atom::Y("hello".into())),
+                                     Sexp::List(vec![Sexp::Atom(Atom::Y("world".into()))])]));
     let s = "(this (has)tight(parens))";
     let s2 = "( this ( has ) tight ( parens ) )";
     assert_eq!(parse(s).unwrap(), parse(s2).unwrap());
